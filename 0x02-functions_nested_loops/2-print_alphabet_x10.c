36b3531f3a359c24e240fdd30ff61897f66fec1b
matin <yemimatin03@gmail.com>
2023-02-26 15:49:03 -0800

    functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function prints 10 times the alphabet, in lowercase
+ * return: always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char i, j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
+
+}
