fb3e7e449c1c0ffc1c3312f1e98519533d451d1f
matin <yemimatin03@gmail.com>
2023-02-27 12:50:11 -0800

    functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - function adds two integers and returns the result
+ *
+ * @a: function parameter a
+ * @b: function parameter b
+ *
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * times_table -  function that prints the 9 times table
+ *
+ */
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i
+}
